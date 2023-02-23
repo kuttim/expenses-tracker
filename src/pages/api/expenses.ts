7b23871e3d939df02b1e0fce74a680532172d7c7
Mikk Küttim <kuttim.mikk@gmail.com>
2023-02-23 23:28:48 +0200

    create expenses api route for adding & deleting

--- src/pages/api/expenses.ts
+++ src/pages/api/expenses.ts
@@ -1,4 +1,8 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 
 interface ExpenseInput {
     title: string;
@@ -9,7 +13,30 @@ interface ExpenseInput {
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-        const { title, description, amount, date, category } = req.body as ExpenseInput;
-    }
+  if (req.method === 'POST') {
+    const { title, description, amount, date, category } = req.body as ExpenseInput;
+
+    const expense = await prisma.expense.create({
+      data: {
+        title,
+        description,
+        amount,
+        date: new Date(date),
+        category,
+      },
+    });
+
+    res.status(201).json(expense);
+  } else if (req.method === 'DELETE') {
+    const id = Number(req.query.id);
+
+    await prisma.expense.delete({
+      where: {
+        id,
+      },
+    });
+
+    res.status(204).end();
+  }
 }
+
